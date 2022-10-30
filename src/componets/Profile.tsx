5f9192283d945915c08df814c8c0a32b6045aa44
genamontana <predator199@yandex.ru>
2022-10-30 15:31:26 +0300

    Viewed 13 Lesson (git basics)

--- src/componets/Profile.tsx
+++ src/componets/Profile.tsx
@@ -6,6 +6,7 @@ export const Profile = () => {
             src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
             alt="image"/></div>
         <div>ava + description</div>
+
         <div>My posts
             <div>New post</div>
             <div>post_1</div>
