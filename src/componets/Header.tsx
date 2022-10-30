ba84f9b9432ba601eac9995363faf4b6b0f39369
genamontana <predator199@yandex.ru>
2022-10-30 17:02:23 +0300

    Viewed 13 Lesson (css-modules)

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import s from './Header.module.css';
 
 
 export const Header = () => {
-    return <header className="header">
-        <img
-            src="https://www.adobe.com/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium"
-            alt="logo-company"/>
+    return <header className={s.header}>
+        <img className={s.header__img}
+             src="https://www.adobe.com/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium"
+             alt="logo-company"/>
     </header>
 }
--- src/componets/Navbar.tsx
+++ src/componets/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import s from './Navbar.module.css';
 
 export const Navbar = () => {
-    return <nav>
-        <div><a>Profile</a></div>
-        <div><a>Messages</a></div>
-        <div><a>News</a></div>
-        <div><a>Music</a></div>
-        <div><a>Setting</a></div>
+    return <nav className={s.nav}>
+        <div className={s.item}><a>Profile</a></div>
+        <div className={`${s.item} ${s.active}`}><a>Messages</a></div>
+        <div className={s.item}><a>News</a></div>
+        <div className={s.item}><a>Music</a></div>
+        <div className={s.item}><a>Setting</a></div>
     </nav>
 }
