73864ce13def26f3c67a816222898db8b870efff
genamontana <predator199@yandex.ru>
2022-11-04 23:57:08 +0300

    Viewed 14 Lesson (folder structure, new components, css improvements)

--- src/componets/Profile/MyPosts/Post/Post.tsx
+++ src/componets/Profile/MyPosts/Post/Post.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import s from './Post.module.css';
+import {Simulate} from 'react-dom/test-utils';
+import input = Simulate.input;
 
 export const Post = () => {
     return <div className={s.item}>
@@ -8,5 +10,9 @@ export const Post = () => {
         <div>
             <span>like</span>
         </div>
+        <input value='5'/>
+        <input value='6'/>
     </div>
+
+
 }
