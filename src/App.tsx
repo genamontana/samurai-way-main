41541395ade2dfc7fff6eb9e3cc59fce8e36d4d5
genamontana <predator199@yandex.ru>
2022-11-05 01:56:36 +0300

    Viewed 19 Lesson (route, browser-router)

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,27 @@ import './App.css';
 import {Dialogs} from './componets/Dialogs/Dialogs';
 import {Header} from './componets/Header/Header';
 import {Navbar} from './componets/Navbar/Navbar';
-//import {Profile} from './componets/Profile/Profile';
-
+import {Profile} from './componets/Profile/Profile';
+import {News} from './componets/News/News';
+import {Music} from './componets/Music/Music';
+import {Settings} from './componets/Settings/Settings';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className="app-wrapper-content">
-                <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/news" component={News}/>
+                    <Route path="/music" component={Music}/>
+                    <Route path="/settings" component={Settings}/>
+                </div>
             </div>
-            {/*<Profile/>*/}
-        </div>
+        </BrowserRouter>
     );
 }
 
