64df071eabe89e576a063b622877bc96d5287b8f
genamontana <predator199@yandex.ru>
2022-11-08 18:57:36 +0300

    Viewed 22 Lesson (Route exact)

--- src/componets/Dialogs/Dialogs.tsx
+++ src/componets/Dialogs/Dialogs.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import s from './Dilogs.module.css'
+import {NavLink} from 'react-router-dom';
 
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Gena
+                    <NavLink to="/dialogs/1">Gena</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Andrey
+                    <NavLink to="/dialogs/2">Andrey</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Svetlana
+                    <NavLink to="/dialogs/3">Svetlana</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Aleksandr
+                    <NavLink to="/dialogs/4">Aleksandr</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Victor
+                    <NavLink to="/dialogs/5">Victor</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Valera
+                    <NavLink to="/dialogs/6">Valera</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hi</div>
-                <div className={s.message}>How is your it-kamasutra?</div>
+                <div className={s.message}>How is your it-incubator?</div>
                 <div className={s.message}>Yo!</div>
             </div>
         </div>
