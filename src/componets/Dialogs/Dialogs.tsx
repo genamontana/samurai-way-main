174fc22c41da2f3e2ef0ac2a59bcdb43977a1339
genamontana <predator199@yandex.ru>
2022-11-08 18:31:22 +0300

    Viewed 21 Lesson (layout of the dialog page)

--- src/componets/Dialogs/Dialogs.tsx
+++ src/componets/Dialogs/Dialogs.tsx
@@ -2,9 +2,33 @@ import React from 'react';
 import s from './Dilogs.module.css'
 
 export const Dialogs = () => {
-  return(
-      <div>
-          Dialogs
-      </div>
-  )
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Gena
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Svetlana
+                </div>
+                <div className={s.dialog}>
+                    Aleksandr
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra?</div>
+                <div className={s.message}>Yo!</div>
+            </div>
+        </div>
+    )
 }
