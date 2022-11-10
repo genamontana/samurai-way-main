369f2960d000a2dbcfe4dfb8d7e76c3fe3f07f09
genamontana <predator199@yandex.ru>
2022-11-10 20:51:27 +0300

    Viewed 25 Lesson (map)

--- src/componets/Dialogs/Dialogs.tsx
+++ src/componets/Dialogs/Dialogs.tsx
@@ -3,9 +3,10 @@ import s from './Dilogs.module.css'
 import {DialogItem} from './DialogItem/DialogItem';
 import {Message} from './Message/Message';
 
+
 export const Dialogs = () => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: '1', name: 'Gena',},
         {id: '2', name: 'Andrey',},
         {id: '3', name: 'Svetlana',},
@@ -14,7 +15,7 @@ export const Dialogs = () => {
         {id: '6', name: 'Valera',},
     ]
 
-    let messagesData = [
+    let messages = [
         {id: '1', message: 'Hi',},
         {id: '2', message: 'How is your it-incubator?',},
         {id: '3', message: 'Yo!',},
@@ -22,21 +23,18 @@ export const Dialogs = () => {
         {id: '5', message: 'Yo!',}
     ]
 
+    let dialogsElements = dialogs.map(d => <DialogItem id={d.id} name={d.name}/>)
+
+    let messagesElements = messages.map(m => <Message message={m.message}/>)
 
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem id={dialogsData[0].id} name={dialogsData[0].name}/>
-                <DialogItem id={dialogsData[1].id} name={dialogsData[1].name}/>
-                <DialogItem id={dialogsData[2].id} name={dialogsData[2].name}/>
-                <DialogItem id={dialogsData[3].id} name={dialogsData[3].name}/>
-                <DialogItem id={dialogsData[4].id} name={dialogsData[4].name}/>
-                <DialogItem id={dialogsData[5].id} name={dialogsData[5].name}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
+                {messagesElements}
             </div>
         </div>
     )
