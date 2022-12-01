1ebf248b418b33ad3d2f65e05cf1d9cf58a35986
genamontana <predator199@yandex.ru>
2022-12-01 20:37:33 +0300

    Viewed 34 Lesson (FLUX)

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -55,10 +55,10 @@ let state: StateType = {
     },
 }
 
-export const addPost = (postMessage: string) => {
+export const addPost = () => {
     const newPost: PostType = {
         id: '5',
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCount: 0,
     }
     state.profilePage.posts.push(newPost);
